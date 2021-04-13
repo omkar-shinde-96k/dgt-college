a262ccc30ae4b213ba98f7986379e09f1280096c
omkar-shinde-96k <oshinde796@gamil.com>
2021-04-13 15:44:45 +0530

    design update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,23 @@
  
 function Slider() {
   return (
-    <>  
-    <img src="images/clg-front-view/2.jpg" width="100%" height="100%" style={{maxHeight:"620px"}}></img>
+    <>   
+    <div className="main">
+                <h1 >Welcome to our college</h1> 
+                <p >
+                Mangaon Shikshan Prasarak Mandal, Mangaon and its President Adv. Rajivji Sable laid a foundation stone of higher education in Mangaon on 1993. It is that time known as Arts’ Senior College. Later It is named as D.G.Tatkare College on 10th January 2010.
+                </p>
+                <br />
+                <div style={{ display: "flex",color:"white" }}>
+                    <a style={{ color:"white", border:"1px solid white"}} href="" className="mybtn text-center">Contact Us</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a style={{ color:"white", border:"1px solid white"}} href="" className="mybtn text-center">About Us</a>
+                </div>
+                <div  className="social-icons">
+                    <a data-aos="zoom-in" data-aos-offset="100" href="https://github.com/omkar-shinde-96k" class="fa fa-github"></a>
+                    <a data-aos="zoom-in" data-aos-offset="100" href="https://www.instagram.com/learn_web_development/" class="fa fa-instagram"></a>
+                    <a data-aos="zoom-in" data-aos-offset="100" href="" class="fa fa-facebook-square"></a>
+                    <a data-aos="zoom-in" data-aos-offset="100" href="" class="fa fa-envelope"></a>
+                </div>
+            </div>
     </>
   );
 } 
