ed5f9e6f1a4122ab6177f5799a9b5496c02a5f38
unknown <hkjava6@gmail.com>
2022-05-27 10:22:28 +0530

    bp

--- src/other-compo/Bestp.js
+++ src/other-compo/Bestp.js
@@ -8,11 +8,555 @@ function Bestp() {
                     <br />
                     <div className="row md-2 events-img">
 
-                        <a className="col-sm-12 col-lg-6" href="../documents/naac/Events/best practices_1.jpg"><img
-                            src="../documents/naac/Events/best practices_1.jpg" /></a>
+                    <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "center"
+    }}
+  >
+    <strong>Title of the Practice</strong>: "
+    <strong>Development of Research Culture among Students and Staff"</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Objectives of the Practice:</strong>
+  </p>
+  <ul style={{ listStyleType: "disc" }}>
+    <li>
+      <span style={{ fontFamily: '"Times New Roman"', fontSize: 16 }}>
+        To make students known with the process of research.
+      </span>
+    </li>
+    <li>
+      <span style={{ fontFamily: '"Times New Roman"', fontSize: 16 }}>
+        To embrace the students for participating and indulging in research.
+      </span>
+    </li>
+    <li>
+      <span style={{ fontFamily: '"Times New Roman"', fontSize: 16 }}>
+        To develop the students into future researchers for further societal
+        benefit.
+      </span>
+    </li>
+  </ul>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Context</strong>:
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    Mangaon region is a culture and bio-diversity rich region. So, there is a
+    huge scope for research in the fields of humanities, commerce and basic
+    sciences. However, due to lack of awareness among the students about the
+    process of research and the scope for research in this region.
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Practice:</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    In the first term of every academic year, lectures on research methodology
+    are conducted separately for the students of each faculty. Through these
+    lectures, we make the students familiar with the process of research.
+    Furthermore, our faculty members continuously motivate and guide the
+    students to select and carry out the projects. We also encourage them to
+    present their projects in district-level inter-collegiate research project
+    presentation competition, Avishkar Research Convention, organized by the
+    university. Staff published their research papers in reputed journals and
+    presume themselves in this culture which college has established.&nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif'
+    }}
+  >
+    &nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif'
+    }}
+  >
+    <strong>Evidence of Success:</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    Due to research friendly culture in the college, number of students taking
+    part in district-level intercollegiate research project presentation
+    competition has been increasing every year. Moreover, the number of projects
+    selected for university-level inter-collegiate research project presentation
+    competition has also been increasing. Along with students we have
+    successfully completed more than 160 plus papers in the span of 5 academic
+    years.&nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Problems encountered and resources required</strong>:
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    Initially, the students did not come forward for research related activity
+    as it was tedious and mind focusing. Our faculty members consistently worked
+    to persuade, encourage, select and guide the students in carrying out
+    research projects. Funding for extensively research project especially for
+    sciences is required. &nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    &nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    &nbsp;
+  </p>
+  <p>
+    <span
+      style={{
+        fontSize: 16,
+        lineHeight: "107%",
+        fontFamily: '"Times New Roman",serif'
+      }}
+    >
+      <br />
+      &nbsp;
+    </span>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "8.0pt",
+      marginLeft: "0cm",
+      lineHeight: "107%",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif'
+    }}
+  >
+    &nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "center"
+    }}
+  >
+    
 
-<a className="col-sm-12 col-lg-6" href="../documents/naac/Events/best practices_2.jpg"><img
-                            src="../documents/naac/Events/best practices_2.jpg" /></a>
+    <strong>Title of the Practice</strong>: "
+    <strong>
+      <u>Teaching with Updates</u>"
+    </strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Objectives of the Practice:</strong>
+  </p>
+  <ul style={{ listStyleType: "disc" }}>
+    <li>
+      <span style={{ fontFamily: '"Times New Roman"', fontSize: 16 }}>
+        To initialize students towards updated teaching content.
+      </span>
+    </li>
+    <li>
+      <span style={{ fontFamily: '"Times New Roman"', fontSize: 16 }}>
+        To enhance the staff overall and holistic knowledge.
+      </span>
+    </li>
+    <li>
+      <span style={{ fontFamily: '"Times New Roman"', fontSize: 16 }}>
+        To make students more advanced in relation to the teaching knowledge and
+        real-life practicality.
+      </span>
+    </li>
+  </ul>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Context</strong>:
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    Knowledge is something which must be updated on regular bases. Imparting
+    knowledge of syllabus is not the only job of an enthusiastic teacher but to
+    teach them in such a manner that they gain maximum theoretical and practical
+    usable knowledge for their future lives.
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Practice:</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    The orientation of the subject begins with knowledge of teacher. Every
+    teacher comes from a detail deep down environment whether somewhere the
+    knowledge that what they got from their teachers was not up to the mark. To
+    relocate this filling teachers have confined themselves to make their
+    students a quality-oriented personality in society. Teaching staff members
+    have taken an initiative along with the guidance of IQAC Coordinator and
+    Principal of College. Staff prepared and explained notes, models, diagrams
+    process of particular topics which are very useful for the students in their
+    job-oriented life.&nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif'
+    }}
+  >
+    &nbsp;
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif'
+    }}
+  >
+    <strong>Evidence of Success:</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    After inculcating into this practice students are more focused towards their
+    work life in terms of preparing themselves for the best which can be seen
+    from their academics and their placement records of college as well as for
+    the appearing in competitive examination.
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>&nbsp;</strong>
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    <strong>Problems encountered and resources required</strong>:
+  </p>
+  <p
+    style={{
+      marginTop: "0cm",
+      marginRight: "0cm",
+      marginBottom: "0cm",
+      marginLeft: "0cm",
+      lineHeight: "normal",
+      fontSize: 16,
+      fontFamily: '"Times New Roman",serif',
+      textAlign: "justify"
+    }}
+  >
+    In the start, the problem faced was related to qualitative information which
+    is being passed on to the students. Staff members were required to put an
+    additional effort for obtaining these notes and passing it in lecture format
+    to the students.
+  </p>
                             
                     </div>
                 </div>
