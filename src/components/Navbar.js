43c846ee9e76daca0cb41a093f919c07057e638f
unknown <hkjava6@gmail.com>
2022-06-08 15:14:07 +0530

    header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import Navlinks from './Navlinks';
 function Navbar({toggle}) { 
     return (
         <>
-             
+             <img src="images/clg-front-view/banner.jpg" alt="" /> 
             <div className="main_section_agile" id="home" style={{ position: "sticky", top: "0px", zIndex: "100", background: "linear-gradient(150deg, blue , rgb(174, 0, 255))" }}>
                 <div className="agileits_w3layouts_banner_nav" id="top" style={{ margin:"0px", padding: "0px 5px 0 30px", width: "100%" }}>
                     <nav className="navbar navbar-default">
                         <div className="navbar-header navbar-left">
-                        <img src="images/clg-front-view/banner.jpg" alt="" /> 
+                        
                             <button type="button" onClick={toggle} className="navbar-toggle collapsed" data-toggle="collapse"
                                 data-target="#bs-example-navbar-collapse-1">
                                 <span className="sr-only">Toggle navigation</span>
