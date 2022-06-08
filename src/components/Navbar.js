918c3c0785bb95e4d93ccc7550b36a3cca40524c
unknown <hkjava6@gmail.com>
2022-06-08 15:28:13 +0530

    header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import Navlinks from './Navlinks';
 function Navbar({toggle}) { 
     return (
         <>
-             <img src="images/clg-front-view/banner.jpg"  style={{ width: "100%" }} alt="" /> 
+             <img src="images/clg-front-view/banner.jpg"  style={{ width: "100%" }} alt=""  className="img-responsive" /> 
             <div className="main_section_agile" id="home" style={{ position: "sticky", top: "0px", zIndex: "100", background: "linear-gradient(150deg, blue , rgb(174, 0, 255))" }}>
                 <div className="agileits_w3layouts_banner_nav" id="top" style={{ margin:"0px", padding: "0px 5px 0 30px", width: "100%" }}>
                     <nav className="navbar navbar-default">
