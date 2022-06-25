6eeb9074e7d0b197d8a09f97418c93214efd3264
Hanzala <hkjava6@gmail.com>
2022-06-25 10:48:09 +0530

    4.1.3

--- src/other-compo/Feedback.js
+++ src/other-compo/Feedback.js
@@ -11,7 +11,14 @@ function Feedback() {
                             <h1 style={{ marginTop: "15px", color: "tomato", textShadow: "3px 3px 5px blue;" }} className="text-center">Feedback Forms
                             </h1>
                             <br />
-
+                            <a className="list-group-item list-group-item-action active .listclass" id="list-home-list"
+                                data-toggle="list" href="https://forms.gle/oUvVNWanz5ErEdkc8" role="tab" aria-controls="home">Alumni Feedback 2021-2022</a>
+                            <a className="list-group-item list-group-item-action active .listclass" id="list-home-list"
+                                data-toggle="list" href="https://forms.gle/p1LgWJSafE4m14XA7" role="tab" aria-controls="home">Students Feedback 2021-2022</a>
+                            <a className="list-group-item list-group-item-action active .listclass" id="list-home-list"
+                                data-toggle="list" href="https://forms.gle/R3ks1dSn1Es1r5Nh6" role="tab" aria-controls="home">Teachers Feedback 2021-2022</a>
+                                <a className="list-group-item list-group-item-action active .listclass" id="list-home-list"
+                                data-toggle="list" href="https://forms.gle/HDEWHUyH9bgH4pFy5" role="tab" aria-controls="home">Parents Feedback 2021-2022</a>    
                             <a className="list-group-item list-group-item-action active .listclass" id="list-home-list"
                                 data-toggle="list" href="https://docs.google.com/forms/d/e/1FAIpQLSfYTN_IKX9p5F38S7pnLW2h55YkeDPOQBd2_qtA0kHeZ3_oqA/viewform" role="tab" aria-controls="home">Anti Ragging Application Form</a>
                             
